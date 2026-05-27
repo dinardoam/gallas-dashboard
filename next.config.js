647af83a5f72d17ca2b8e1fbe3860b5a46356fa5
Anthony DiNardo <anthony.dinardo@henrisbakery.com>
2026-05-26 23:03:56 -0400

    Switch to static export, remove API routes

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
+  images: { unoptimized: true },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 }
 
 module.exports = nextConfig
